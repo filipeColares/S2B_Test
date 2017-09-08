e6f7c7114b8ceae19cbba8c5df5bd96bc4475d09
teste <teste@10.32.175.44>
2017-09-08 16:40:11 -0300

    Versão com a release versão

--- somador/src/main/java/s2bTeste/somador/App.java
+++ somador/src/main/java/s2bTeste/somador/App.java
@@ -9,13 +9,15 @@ public class App
 {
     public static void main( String[] args )
     {
-    	double i = 0;
+    	double i = 4;
     	double j = 5;
     	double s = somador.somador(i, j);
         
     	System.out.println( "Hello World!" );
     	System.out.println(i);
     	System.out.println(j);
+    	System.out.println("Resultado");
+    	System.out.print(s);
     	
     }
 }
